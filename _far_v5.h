714596dba8b6b7362634249e8a8c571e5431adc1
Jean-Baptiste Note <jean-baptiste.note@m4x.org>
2007-02-21 13:26:14 +0100

    [virtex4/5] syntactic sharing of bitstream parsing code
    Signed-off-by: Jean-Baptiste Note <jean-baptiste.note@m4x.org>

---
+++ _far_v5.h
@@ -0,0 +1,301 @@
+#include "bitstream_v5.h"
+
+static const unsigned
+v5_frame_count[V5C__NB_CFG] = {
+  [V5C_IOB] = 54,
+  [V5C_CLB] = 36,
+  [V5C_DSP48] = 28,
+  [V5C_GCLK] = 4,
+  [V5C_BRAM_INT] = 30,
+  [V5C_BRAM] = 128,
+  [V5C_PAD] = 2,
+};
+
+static const
+chip_struct_t bitdescr[XC5VLX__NUM] = {
+  /* FLR is always 41 for virtex-5 */
+  [XC5VLX30] = { .idcode = 0x286E093,
+		 .framelen = 41,
+		 .frame_count = v5_frame_count,
+		 .col_count = {
+		   [V5_TYPE_CLB] = XC5VLX30_NFRAMES,
+		   [V5_TYPE_BRAM] = XC5VLX30_NBRAMS,
+		 },
+		 .col_type = xc5vlx30,
+		 .row_count = 2,
+  },
+  [XC5VLX50] = { .idcode = 0x2896093,
+		 .framelen = 41,
+		 .frame_count = v5_frame_count,
+		 .col_count = {
+		   [V5_TYPE_CLB] = XC5VLX30_NFRAMES,
+		   [V5_TYPE_BRAM] = XC5VLX30_NBRAMS,
+		 },
+		 .col_type = xc5vlx30,
+		 .row_count = 3,
+  },
+  [XC5VLX85] = { .idcode = 0x28AE093,
+		 .framelen = 41,
+		 .frame_count = v5_frame_count,
+		 .col_count = {
+		   [V5_TYPE_CLB] = XC5VLX85_NFRAMES,
+		   [V5_TYPE_BRAM] = XC5VLX85_NBRAMS,
+		 },
+		 .col_type = xc5vlx85,
+		 .row_count = XC5VLX85_NROWS,
+  },
+  [XC5VLX110] = { .idcode = 0x28D6093,
+		  .framelen = 41,
+		  .frame_count = v5_frame_count,
+		  .col_count = {
+		    [V5_TYPE_CLB] = XC5VLX85_NFRAMES,
+		    [V5_TYPE_BRAM] = XC5VLX85_NBRAMS,
+		  },
+		  .col_type = xc5vlx85,
+		  .row_count = XC5VLX85_NROWS + 1,
+  },
+  [XC5VLX220] = { .idcode = 0x290C093,
+		  .framelen = 41,
+		  .frame_count = v5_frame_count,
+		  .col_count = {
+		    [V5_TYPE_CLB] = XC5VLX220_NFRAMES,
+		    [V5_TYPE_BRAM] = XC5VLX220_NBRAMS,
+		  },
+		  .col_type = xc5vlx220,
+		  .row_count = XC5VLX220_NROWS,
+  },
+  /* not verified, I cannot synthetize for this chip */
+  [XC5VLX330] = { .idcode = 0x295C093,
+		  .framelen = 41,
+		  .frame_count = v5_frame_count,
+		  .col_count = {
+		    [V5_TYPE_CLB] = XC5VLX220_NFRAMES,
+		    [V5_TYPE_BRAM] = XC5VLX220_NBRAMS,
+		  },
+		  .col_type = xc5vlx220,
+		  .row_count = XC5VLX220_NROWS + 1,
+  },
+};
+
+typedef enum _ba_v5_col_type {
+  BA_TYPE_CLB = 0,
+  /* Error in UG191, I guess */
+  BA_TYPE_BRAM_INT,
+  BA_TYPE_BRAM,
+} ba_v5_col_type_t;
+
+typedef enum parser_state {
+  STATE_IDLE = 0,
+  STATE_UNSYNCHED,
+  STATE_WAITING_CTRL,
+  STATE_WAITING_DATA,
+} parser_state_t;
+
+typedef enum _v5_registers_index {
+  /* from xilinx ug071.pdf */
+  CRC = 0, FAR, FDRI, FDRO,
+  CMD, CTL0, MASK,
+  STAT, LOUT, COR0,
+  MFWR, CBC, IDCODE,
+  AXSS, COR1, CSOB, WBSTAR,
+  TIMER,
+  BOOTSTS = 22,
+  CTL1 = 24,
+  __V5_NUM_REGISTERS,
+} register_index_t;
+
+typedef enum _cmd_code {
+  CMD_NULL = 0,
+  WCFG, MFW,
+  DGHIGH_LFRM, RCFG, START, RCAP, RCRC,
+  AGHIGH, SWITCH, GRESTORE,
+  SHUTDOWN, GCAPTURE,
+  DESYNCH, RESERVED0, IPROG,
+  CRCC, LTIMER,
+  __NUM_CMD_CODE,
+} cmd_code_t;
+
+#if DEBIT_DEBUG > 0
+static const
+char *reg_names[__V5_NUM_REGISTERS] = {
+  [CRC] = "CRC",
+  [FAR] = "FAR",
+  [FDRI] = "FDRI",
+  [FDRO] = "FDRO",
+  [CMD] = "CMD",
+  [CTL0] = "CTL0",
+  [MASK] = "MASK",
+  [STAT] = "STAT",
+  [LOUT] = "LOUT",
+  [COR0] = "COR0",
+  [MFWR] = "MFWR",
+  [CBC] = "CBC",
+  [IDCODE] = "IDCODE",
+  [AXSS] = "AXSS",
+  [COR1] = "COR1",
+  [CSOB] = "CSOB",
+  [WBSTAR] = "WBSTAR",
+  [TIMER] = "TIMER",
+  [BOOTSTS] = "BOOTSTS",
+  [CTL1] = "CTL1",
+};
+
+static const
+char *cmd_names[__NUM_CMD_CODE] = {
+  [CMD_NULL] = "NULL",
+  [WCFG] = "WCFG",
+  [MFW] = "MFWR",
+  [DGHIGH_LFRM] = "LFRM",
+  [RCFG] = "RCFG",
+  [START] = "START",
+  [RCAP] = "RCAP",
+  [RCRC] = "RCRC",
+  [AGHIGH] = "AGHIGH",
+  [SWITCH] = "SWITCH",
+  [GRESTORE] = "GRESTORE",
+  [SHUTDOWN] = "SHUTDOWN",
+  [GCAPTURE] = "GCAPTURE",
+  [DESYNCH] = "DESYNCH",
+};
+#endif
+
+#define sw_far_t sw_far_v5_t
+#define id_vlx_t id_v5vlx_t
+#define col_type_t v5_col_type_t
+#define design_col_t v5_design_col_t
+#define XC_VLX__NUM XC5VLX__NUM
+#define VC__NB_CFG V5C__NB_CFG
+#define get_hwfar get_hwfar_v5
+#define fill_swfar fill_swfar_v5
+#define frame_count_v v5_frame_count
+
+/* This structure contains the internal structure of the parser */
+typedef struct _bitstream_parser {
+  /* state of the parser */
+  parser_state_t state;
+  /* state of the register engine */
+  register_index_t active_register;
+  gint write__read;
+  gint active_length;
+
+  /* Actual state of the various registers */
+  xil_register_t registers[__V5_NUM_REGISTERS];
+
+  /* detailed view of some registers */
+  id_vlx_t type;
+
+  /* Specific FDRI quirks */
+  const void *last_frame;
+
+  /* Bitstream proper */
+  bytearray_t ba;
+} bitstream_parser_t;
+
+/*
+ * Length, in words, of a frame.
+ */
+
+#define frame_length 41
+
+/***
+ * FAR handling
+ */
+
+static inline void _far_increment_type(sw_far_t *addr);
+static inline gboolean _far_is_pad(const bitstream_parser_t *bitstream,
+				   const sw_far_t *addr);
+
+static inline void
+_far_increment_tb(sw_far_t *addr) {
+  v5_tb_t tb = addr->tb;
+  switch (tb) {
+  case V5_TB_TOP:
+    addr->tb = V5_TB_BOTTOM;
+    break;
+  case V5_TB_BOTTOM:
+    addr->tb = V5_TB_TOP;
+    /* increase type */
+    _far_increment_type(addr);
+    break;
+  default:
+    g_assert_not_reached();
+  }
+}
+
+int
+snprintf_far(char *buf, const size_t buf_len,
+	     const uint32_t hwfar) {
+  return snprintf(buf, buf_len,
+		  "%i_%i_%i_%i_%i",
+		  v5_tb_of_far(hwfar),
+		  v5_type_of_far(hwfar),
+		  v5_row_of_far(hwfar),
+		  v5_col_of_far(hwfar),
+		  v5_mna_of_far(hwfar));
+}
+
+/* Type is a bit strange -- it watches the type from the far and the col
+   count, so as to get a mixed type which depends on both these
+   parameters. */
+
+static inline design_col_t
+_type_of_far(const bitstream_parser_t *bitstream,
+	     const sw_far_t *addr) {
+  const id_vlx_t chiptype = bitstream->type;
+  const col_type_t type = addr->type;
+
+  if (_far_is_pad(bitstream, addr))
+    return V5C_PAD;
+
+  switch (type) {
+  case V5_TYPE_CLB: {
+    const unsigned col = addr->col;
+    return bitdescr[chiptype].col_type[col];
+  }
+  case V5_TYPE_BRAM:
+    return V5C_BRAM;
+  case V5_TYPE_CFG_CLB:
+    g_print("Unknown frame type, please report your bitstream");
+  default:
+    g_assert_not_reached();
+  }
+
+  return -1;
+}
+
+static inline unsigned
+typed_col_dumb(const design_col_t *array,
+	       const unsigned col) {
+  const design_col_t type = array[col];
+  unsigned i, count = 0;
+
+  for (i = 0; i < col; i++)
+    if (array[i] == type)
+      count++;
+
+  return count;
+}
+
+
+static inline unsigned
+_typed_col_of_far(const bitstream_parser_t *bitstream,
+		  const sw_far_t *addr) {
+  const id_vlx_t chiptype = bitstream->type;
+  const col_type_t type = addr->type;
+  const unsigned col = addr->col;
+
+  /* unlikely, move this out of main exec flow */
+  if (_far_is_pad(bitstream, addr))
+    return type;
+
+  switch (type) {
+  case V5_TYPE_CLB: {
+    const design_col_t *typear = bitdescr[chiptype].col_type;
+    return typed_col_dumb(typear, col);
+  }
+  default:
+    return col;
+  }
+
+  return -1;
+}
--- bitstream_parser_common.c
+++ bitstream_parser_common.c
@@ -1,7 +1,7 @@
 /*
  * (C) Copyright 2006 Jean-Baptiste Note <jean-baptiste.note@m4x.org>
  * All rights reserved.
- * Virtex4 bitstream parser.
+ * Common Virtex4/Virtex5 bitstream parser.
  */
 
 #include <string.h>
@@ -11,7 +11,7 @@
 
 #include "bitarray.h"
 #include "bitstream_packets.h"
-#include "design_v4.h"
+#include "design.h"
 #include "bitheader.h"
 #include "bitstream_parser.h"
 #include "debitlog.h"
@@ -19,212 +19,15 @@
 #include "codes/crc32-c.h"
 #include "codes/xhamming.h"
 
-static const unsigned
-v4_frame_count[V4C__NB_CFG] = {
-  [V4C_IOB] = 30,
-  [V4C_GCLK] = 3,
-  [V4C_CLB] = 22,
-  [V4C_DSP48] = 21,
-  [V4C_BRAM] = 64,
-  [V4C_BRAM_INT] = 20,
-  /* the padding frames are seen as a compulsory column of two frames at
-     the end of any row for all three types of columns */
-  [V4C_PAD] = 2,
-};
-
-static const
-chip_struct_t bitdescr[XC4VLX__NUM] = {
-  /* FLR is always 41 for virtex-4 */
-  [XC4VLX15] = { .idcode = 0x01658093U,
-		 .chip = XC4VLX15,
-		 .framelen = 41,
-		 .frame_count = v4_frame_count,
-		 .col_count = {
-		   [V4_TYPE_CLB] = 29,
-		   [V4_TYPE_BRAM] = 3,
-		   [V4_TYPE_BRAM_INT] = 3,
-		 },
-		 .row_count = 2, },
-  [XC4VLX25] = { .idcode = 0x0167C093,
-		 .chip = XC4VLX25,
-		 .framelen = 41,
-		 .frame_count = v4_frame_count,
-		 .col_count = {
-		   [V4_TYPE_CLB] = 33,
-		   [V4_TYPE_BRAM] = 3,
-		   [V4_TYPE_BRAM_INT] = 3,
-		 },
-		 .row_count = 3, },
-  [XC4VLX40] = { .idcode = 0x016A4093U,
-		 .chip = XC4VLX40,
-		 .framelen = 41,
-		 .frame_count = v4_frame_count,
-		 .col_count = {
-		   [V4_TYPE_CLB] = 41,
-		   [V4_TYPE_BRAM] = 3,
-		   [V4_TYPE_BRAM_INT] = 3,
-		 },
-		 .row_count = 4, },
-  [XC4VLX60] = { .idcode = 0x016B4093U,
-		 .chip = XC4VLX60,
-		 .framelen = 41,
-		 .frame_count = v4_frame_count,
-		 .col_count = {
-		   [V4_TYPE_CLB] = 57,
-		   [V4_TYPE_BRAM] = 5,
-		   [V4_TYPE_BRAM_INT] = 5,
-		 },
-		 .row_count = 4, },
-  [XC4VLX80] = { .idcode = 0x016D8093U,
-		 .chip = XC4VLX80,
-		 .framelen = 41,
-		 .frame_count = v4_frame_count,
-		 .col_count = {
-		   [V4_TYPE_CLB] = 61,
-		   [V4_TYPE_BRAM] = 5,
-		   [V4_TYPE_BRAM_INT] = 5,
-		 },
-		 .row_count = 5, },
-  [XC4VLX100] = { .idcode = 0x01700093U,
-		  .chip = XC4VLX100,
-		  .framelen = 41,
-		  .frame_count = v4_frame_count,
-		  .col_count = {
-		    [V4_TYPE_CLB] = 69,
-		    [V4_TYPE_BRAM] = 5,
-		    [V4_TYPE_BRAM_INT] = 5,
-		  },
-		  .row_count = 6, },
-  [XC4VLX160] = { .idcode = 0x01718093U,
-		  .chip = XC4VLX160,
-		  .framelen = 41,
-		  .frame_count = v4_frame_count,
-		  .col_count = {
-		    [V4_TYPE_CLB] = 93,
-		    [V4_TYPE_BRAM] = 6,
-		    [V4_TYPE_BRAM_INT] = 6,
-		  },
-		  .row_count = 6, },
-  [XC4VLX200] = { .idcode = 0x01734093U,
-		  .chip = XC4VLX200,
-		  .framelen = 41,
-		  .frame_count = v4_frame_count,
-		  .col_count = {
-		    [V4_TYPE_CLB] = 121,
-		    [V4_TYPE_BRAM] = 7,
-		    [V4_TYPE_BRAM_INT] = 7,
-		  },
-		  .row_count = 6, },
-};
-
-typedef enum _ba_v4_col_type {
-  BA_TYPE_CLB = 0,
-  BA_TYPE_BRAM_INT,
-  BA_TYPE_BRAM,
-} ba_v4_col_type_t;
-
-typedef enum parser_state {
-  STATE_IDLE = 0,
-  STATE_UNSYNCHED,
-  STATE_WAITING_CTRL,
-  STATE_WAITING_DATA,
-} parser_state_t;
-
-typedef enum _v4_registers_index {
-  /* from xilinx ug071.pdf */
-  CRC = 0, FAR, FDRI, FDRO,
-  CMD, CTL, MASK,
-  STAT, LOUT, COR,
-  MFWR, CBC, IDCODE,
-  AXSS,
-  __V4_NUM_REGISTERS,
-} register_index_t;
-
-typedef enum _cmd_code {
-  CMD_NULL = 0,
-  WCFG, MFW,
-  LFRM, RCFG, START, RCAP, RCRC,
-  AGHIGH, SWITCH, GRESTORE,
-  SHUTDOWN, GCAPTURE,
-  DESYNCH,
-  __NUM_CMD_CODE,
-} cmd_code_t;
-
-#if DEBIT_DEBUG > 0
-static const
-char *reg_names[__V4_NUM_REGISTERS] = {
-  [CRC] = "CRC",
-  [FAR] = "FAR",
-  [FDRI] = "FDRI",
-  [FDRO] = "FDRO",
-  [CMD] = "CMD",
-  [CTL] = "CTL",
-  [MASK] = "MASK",
-  [STAT] = "STAT",
-  [LOUT] = "LOUT",
-  [COR] = "COR",
-  [MFWR] = "MFWR",
-  [CBC] = "CBC",
-  [IDCODE] = "IDCODE",
-  [AXSS] = "AXSS",
-};
-
-static const
-char *cmd_names[__NUM_CMD_CODE] = {
-  [CMD_NULL] = "NULL",
-  [WCFG] = "WCFG",
-  [MFW] = "MFWR",
-  [LFRM] = "LFRM",
-  [RCFG] = "RCFG",
-  [START] = "START",
-  [RCAP] = "RCAP",
-  [RCRC] = "RCRC",
-  [AGHIGH] = "AGHIGH",
-  [SWITCH] = "SWITCH",
-  [GRESTORE] = "GRESTORE",
-  [SHUTDOWN] = "SHUTDOWN",
-  [GCAPTURE] = "GCAPTURE",
-  [DESYNCH] = "DESYNCH",
-};
+#if defined(VIRTEX4)
+#include "_far_v4.h"
+#elif defined(VIRTEX5)
+#include "_far_v5.h"
 #endif
 
-#define sw_far_t sw_far_v4_t
-#define id_vlx_t id_v4vlx_t
-#define col_type_t v4_col_type_t
-#define design_col_t v4_design_col_t
-#define XC_VLX__NUM XC4VLX__NUM
-#define VC__NB_CFG V4C__NB_CFG
-#define get_hwfar get_hwfar_v4
-#define fill_swfar fill_swfar_v4
-#define frame_count_v v4_frame_count
-
-/* This structure contains the internal structure of the parser */
-typedef struct _bitstream_parser {
-  /* state of the parser */
-  parser_state_t state;
-  /* state of the register engine */
-  register_index_t active_register;
-  gint write__read;
-  gint active_length;
-
-  /* Actual state of the various registers */
-  xil_register_t registers[__V4_NUM_REGISTERS];
-
-  /* detailed view of some registers */
-  id_vlx_t type;
-
-  /* Specific FDRI quirks */
-  const void *last_frame;
-
-  /* Bitstream proper */
-  bytearray_t ba;
-} bitstream_parser_t;
-
-/*
- * Length, in words, of a frame.
+/*****
+ * Common parsing code for virtex4 and virtex5 bitstreams
  */
-
-#define frame_length 41
 
 /***
  * Raw register IO
@@ -283,18 +86,6 @@ update_crc(bitstream_parser_t *parser,
  * FAR handling
  */
 
-int
-snprintf_far(char *buf, const size_t buf_len,
-	     const uint32_t hwfar) {
-  return snprintf(buf, buf_len,
-		  "%i_%i_%i_%i_%i",
-		  v4_tb_of_far(hwfar),
-		  v4_type_of_far(hwfar),
-		  v4_row_of_far(hwfar),
-		  v4_col_of_far(hwfar),
-		  v4_mna_of_far(hwfar));
-}
-
 void
 typed_frame_name(char *buf, unsigned buf_len,
 		 const unsigned type,
@@ -314,23 +105,6 @@ print_far(bitstream_parser_t *parser) {
 static inline void
 _far_increment_type(sw_far_t *addr) {
   addr->type++;
-}
-
-static inline void
-_far_increment_tb(sw_far_t *addr) {
-  v4_tb_t tb = addr->tb;
-  switch (tb) {
-  case V4_TB_TOP:
-    addr->tb = V4_TB_BOTTOM;
-    break;
-  case V4_TB_BOTTOM:
-    addr->tb = V4_TB_TOP;
-    /* increase type */
-    _far_increment_type(addr);
-    break;
-  default:
-    g_assert_not_reached();
-  }
 }
 
 static inline void
@@ -389,53 +163,6 @@ far_is_pad(bitstream_parser_t *bitstream, guint32 myfar) {
   sw_far_t far;
   fill_swfar(&far, myfar);
   return _far_is_pad(bitstream, &far);
-}
-
-/* Type is a bit strange -- it watches the type from the far and the col
-   count, so as to get a mixed type which depends on both these
-   parameters. */
-#define DSP_V4_OF_END(x) ((x) > 50 ? 13 : 9)
-
-static inline design_col_t
-_type_of_far(const bitstream_parser_t *bitstream,
-	     const sw_far_t *addr) {
-  const id_vlx_t chiptype = bitstream->type;
-  const col_type_t type = addr->type;
-
-  /* unlikely */
-  if (_far_is_pad(bitstream, addr))
-    return V4C_PAD;
-
-  switch (type) {
-  case V4_TYPE_CLB:
-    {
-      const unsigned col = addr->col;
-      const unsigned end = bitdescr[chiptype].col_count[V4_TYPE_CLB] - 1;
-      const unsigned middle = end >> 1;
-      const unsigned dsp = DSP_V4_OF_END(end);
-      /* Let's be more intelligent.
-	 Middle, extremities: IO.
-	 DSP is at fixed position and the rest is CLB */
-      if (col == 0 || col == end || col == middle)
-	return V4C_IOB;
-      if (col == (middle + 1))
-	return V4C_GCLK;
-      if (col == dsp)
-	return V4C_DSP48;
-      return V4C_CLB;
-    }
-  case V4_TYPE_BRAM:
-    return V4C_BRAM;
-  case V4_TYPE_BRAM_INT:
-    return V4C_BRAM_INT;
-  case V4_TYPE_CFG_CLB:
-  case V4_TYPE_CFG_BRAM:
-    g_print("Unknown frame type, please report your bitstream");
-  default:
-    g_assert_not_reached();
-  }
-
-  return -1;
 }
 
 static inline void
@@ -505,48 +232,6 @@ default_register_write(bitstream_parser_t *parser,
 
 }
 
-static inline unsigned
-_typed_col_of_far(const bitstream_parser_t *bitstream,
-		  const sw_far_t *addr) {
-  const id_vlx_t chiptype = bitstream->type;
-  const col_type_t type = addr->type;
-  const unsigned col = addr->col;
-
-  /* unlikely, move this out of main exec flow */
-  if (_far_is_pad(bitstream, addr))
-    return type;
-
-  switch (type) {
-  case V4_TYPE_CLB:
-    {
-      const unsigned col = addr->col;
-      const unsigned end = bitdescr[chiptype].col_count[V4_TYPE_CLB] - 1;
-      const unsigned middle = end >> 1;
-      const unsigned dsp = DSP_V4_OF_END(end);
-
-      if (col == 0)
-	return 0;
-      if (col == dsp)
-	return 0;
-      if (col == middle)
-	return 1;
-      if (col == end)
-	return 2;
-      if (col == (middle + 1))
-	return 0;
-      if (col < dsp)
-	return col-1;
-      if (col < middle)
-	return col-2;
-      return col-4;
-    }
-  default:
-    return col;
-  }
-
-  return -1;
-}
-
 static inline const gchar **
 get_frameloc_from_far(const bitstream_parsed_t *parsed,
 		      const bitstream_parser_t *parser,
@@ -560,7 +245,7 @@ get_frameloc_from_far(const bitstream_parsed_t *parsed,
   return get_frame_loc(parsed, type, far.row, far.tb, typed_col, far.mna);
 }
 
-static inline
+static
 void record_frame(bitstream_parsed_t *parsed,
 		  bitstream_parser_t *bitstream,
 		  const guint32 myfar) {
@@ -1008,3 +693,4 @@ free_bitstream(bitstream_parsed_t *bitstream) {
   g_free(bitstream);
   return;
 }
+
